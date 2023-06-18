293c7c9ca6d414160a00ce90e9b42188c2858263
cj187 <73111604+cj187@users.noreply.github.com>
2023-06-18 16:37:42 +0800

    datafusion change

--- src/views/dataFusion/dataFusion.vue
+++ src/views/dataFusion/dataFusion.vue
@@ -19,7 +19,7 @@ const taskData = reactive({
   lists: "",
 })
 let tableData = reactive({
-  filterData:[]
+  filterData: []
 })
 
 
@@ -42,7 +42,6 @@ function handleCurrentChange(val) {
   // 改变默认的页数
   this.state.currentPage = val
 }
-
 
 
 let myChart = null;
@@ -70,6 +69,115 @@ onMounted(async () => {
 
   //绘制ECharts关系图
   myChart.setOption({
+    // graphic: [
+    //   {
+    //     type: 'polygon',
+    //     shape: {
+    //       points: calculatePoints([[100, 70], [750, 70], [700, 200], [50, 200]])  //左上、右上、右下、左下
+    //     },
+    //     style: {
+    //       fill: '#eee',
+    //       stroke: 'red',
+    //       lineWidth: 3,
+    //       opacity: 0.3, //透明度
+    //       shadowBlur: 10, //边框阴影模糊程度
+    //       shadowColor: 'gray', //边框阴影颜色
+    //       shadowOffsetX: 5, //边框阴影水平偏移量
+    //       shadowOffsetY: 5 //边框阴影垂直偏移量
+    //     },
+    //     text: {
+    //       type: 'text',
+    //       style: {
+    //         text: ' 汽车产业链',  // 你要显示的文字
+    //         x: 700,  // 文字的横坐标
+    //         y: 310,   // 文字的纵坐标
+    //         fill: 'black',  // 文字颜色
+    //         fontSize: 20  // 文字大小
+    //       }
+    //     }
+    //   },
+    //   {
+    //     type: 'polygon',
+    //     shape: {
+    //       points: calculatePoints([[50, 440], [700, 440], [750, 310], [100, 310]])  //左下、右下、右上、左上
+    //     },
+    //     style: {
+    //       fill: '#eee',
+    //       stroke: 'blue',
+    //       lineWidth: 3,
+    //       opacity: 0.3,
+    //       shadowBlur: 10,
+    //       shadowColor: 'gray',
+    //       shadowOffsetX: 5,
+    //       shadowOffsetY: 5
+    //     }
+    //   },
+    //
+    // ],
+    graphic: [
+      {
+        type: 'group',
+        position: [100, 70], // 左上角位置
+        children: [
+          {
+            type: 'polygon',
+            shape: {
+              points: calculatePoints([[0, -10], [650, -10], [600, 110], [-50, 110]])  // 左上、右上、右下、左下
+            },
+            style: {
+              fill: '#eee',
+              stroke: 'red',
+              lineWidth: 3,
+              opacity: 0.3,
+              shadowBlur: 10,
+              shadowColor: 'gray',
+              shadowOffsetX: 5,
+              shadowOffsetY: 5
+            }
+          },
+          {
+            type: 'text',
+            position: [-50, 70], // 相对于 group 左上角位置的偏移量
+            style: {
+              text: '汽车产业链', // 你要显示的文字
+              fill: 'black', // 文字颜色
+              fontSize: 14 // 文字大小
+            }
+          }
+        ]
+      },
+      {
+        type: 'group',
+        position: [50, 310], // 左上角位置
+        children: [
+          {
+            type: 'polygon',
+            shape: {
+              points: calculatePoints([[0, 30], [650, 30], [700, -100], [50, -100]])  // 左下、右下、右上、左上
+            },
+            style: {
+              fill: '#eee',
+              stroke: 'blue',
+              lineWidth: 3,
+              opacity: 0.3,
+              shadowBlur: 10,
+              shadowColor: 'gray',
+              shadowOffsetX: 5,
+              shadowOffsetY: 5
+            }
+          },
+          {
+            type: 'text',
+            position: [15, 5], // 相对于 group 左上角位置的偏移量
+            style: {
+              text: '家电产业链', // 你要显示的文字
+              fill: 'black', // 文字颜色
+              fontSize: 14 // 文字大小
+            }
+          }
+        ]
+      }
+    ],
 
     title: {
       // text: '多重产业链网络图',
@@ -77,6 +185,7 @@ onMounted(async () => {
       top: 'bottom',
       left: 'right'
     },
+
 
     tooltip: {
       show: true,
@@ -92,7 +201,13 @@ onMounted(async () => {
           // return "id: " + id + '<br/>' + "企业：" + name + '<br/>' + "所处领域：" + filed + '<br/>' + '所处产业链: ' + category + '<br/>' + '所处网络: ' + network;
           var id = params.data.id;
           var name = params.data.name;
-          return   "任务：" + name;
+          var requirements = params.data.requirements;
+          var products = params.data.products;
+          var deadline = params.data.deadline;
+          var lastTime = params.data.lastTime;
+          var numbers = params.data.numbers;
+          var lists = params.data.lists;
+          return "id: " + id + '<br/>' + "任务名称：" + name + '<br/>' + "资源需求：" + requirements + '<br/>' + "预期产量：" + products + '<br/>' + "截止时间：" + deadline + '<br/>' + "预估耗时：" + lastTime + '<br/>' + "参与企业数量：" + numbers + '<br/>' + "参与企业名单：" + lists;
         }
         // else if (params.dataType === 'edge') {
         //     var source = params.data.source;
@@ -115,7 +230,7 @@ onMounted(async () => {
       type: 'graph',
       layout: 'none',
       data: graph.nodes,
-      // links: graph.links,
+      links: graph.links,
       categories: graph.categories,
       //network: graph.network,
       roam: true,     //开启鼠标缩放和平移漫游
@@ -125,19 +240,29 @@ onMounted(async () => {
         // position: 'right',  //节点标签的位置
         formatter: '{b}'  //节点标签的内容格式器，a 代表系列名，b 代表数据名，c 代表数据值。
       },
-      // lineStyle: {
-      //     normal: {
-      //         color: 'source',    //边的颜色是由源节点决定的
-      //         curveness: 0.4  //边的曲度，支持从 0 到 1 的值，值越大曲度越大。
-      //     }
-      // },
+      edgeSymbol: ['none', 'arrow'],
+      edgeSymbolSize: [4, 6],
+      lineStyle: {
+        color: 'source',
+        curveness: 0.2,
+        width: 2,
+        type: 'solid',
+        arrow: {
+          show: true,
+          size: 6,
+          offset: 0,
+          symbol: 'arrow',
+          symbolSize: [6, 12]
+        }
+      },
+
       emphasis: {
         focus: 'adjacency', //当鼠标移动到节点上，突出显示节点以及节点的边和邻接节点，'adjacency' 表示只突出显示节点以及节点的边
         lineStyle: {    //边的设置
           width: 10,
           type: "dotted",
           // shadowBlur:0.5,
-          opacity: 0.7
+          opacity: 0.7,
         }
       },
     }]
@@ -251,95 +376,95 @@ function searchTasks() {
 
 <template>
   <div>
-  <div class="topicText">
-    <h2>数据融合</h2>
-  </div>
-  <el-tabs style="margin-left:20px">
-    <el-tab-pane label="任务划分">
-      <br/>
+    <div class="topicText">
+      <h2>群智汇聚</h2>
+    </div>
+    <el-tabs style="margin-left:20px">
+      <el-tab-pane label="任务划分">
+        <br/>
 
-      <el-row style="margin-bottom:20px;">
-        <el-col :span="12" style="margin-left:40px">
-          <div id="main" style="width: 400px;height:400px;background-color: white"></div>
-        </el-col>
+        <el-row style="margin-bottom:20px;">
+          <el-col :span="12" style="margin-left:40px">
+            <div id="main" style="width: 1000px;height:400px;margin-left:100px;background-color: rgba(250,247,247,0.5)"></div>
+          </el-col>
 
-        <el-col :span="10">
-          <el-card class="box-card" style="width: 400px;height:400px">
-            <template #header>
-              <div class="card-header">
-                <span>任务简介</span>
-              </div>
-            </template>
-            <div class="text item">
-              {{ '任务名称: ' + taskData.name }}
-            </div>
-            <div class="text item">
-              {{ '资源需求: ' + taskData.requirements }}
-            </div>
-            <div class="text item">
-              {{ '预期产量: ' + taskData.products }}
-            </div>
-            <div class="text item">
-              {{ '截止时间: ' + taskData.deadline }}
-            </div>
-            <div class="text item">
-              {{ '预估耗时: ' + taskData.lastTime }}
-            </div>
-            <div class="text item">
-              {{ '参与企业数量: ' + taskData.numbers }}
-            </div>
-            <div class="text item">
-              {{ '参与企业名单: ' + taskData.lists }}
-            </div>
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-tab-pane>
+<!--          <el-col :span="10">-->
+<!--            <el-card class="box-card" style="width: 400px;height:400px;margin-left: 20px">-->
+<!--              <template #header>-->
+<!--                <div class="card-header">-->
+<!--                  <span>任务简介</span>-->
+<!--                </div>-->
+<!--              </template>-->
+<!--              <div class="text item">-->
+<!--                {{ '任务名称: ' + taskData.name }}-->
+<!--              </div>-->
+<!--              <div class="text item">-->
+<!--                {{ '资源需求: ' + taskData.requirements }}-->
+<!--              </div>-->
+<!--              <div class="text item">-->
+<!--                {{ '预期产量: ' + taskData.products }}-->
+<!--              </div>-->
+<!--              <div class="text item">-->
+<!--                {{ '截止时间: ' + taskData.deadline }}-->
+<!--              </div>-->
+<!--              <div class="text item">-->
+<!--                {{ '预估耗时: ' + taskData.lastTime }}-->
+<!--              </div>-->
+<!--              <div class="text item">-->
+<!--                {{ '参与企业数量: ' + taskData.numbers }}-->
+<!--              </div>-->
+<!--              <div class="text item">-->
+<!--                {{ '参与企业名单: ' + taskData.lists }}-->
+<!--              </div>-->
+<!--            </el-card>-->
+<!--          </el-col>-->
+        </el-row>
+      </el-tab-pane>
 
-    <el-tab-pane label="任务查询">
-      <el-row>
-        <el-form :inline="true" :model="formInline" class="demo-form-inline">
-          <el-form-item label="聚类基准">
-            <el-input v-model="formInline.user" placeholder="紧急性"/>
-          </el-form-item>
-          <el-form-item label="紧急性查询">
-            <el-select v-model="formInline.region" placeholder="类别查询">
-              <el-option label="紧急性1" value="紧急性1"/>
-              <el-option label="紧急性2" value="紧急性2"/>
-              <el-option label="紧急性3" value="紧急性3"/>
-              <el-option label="紧急性4" value="紧急性4"/>
-              <el-option label="紧急性5" value="紧急性5"/>
-            </el-select>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="searchTasks">查询</el-button>
-          </el-form-item>
-        </el-form>
-      </el-row>
+      <el-tab-pane label="任务查询">
+        <el-row>
+          <el-form :inline="true" :model="formInline" class="demo-form-inline">
+            <el-form-item label="聚类基准">
+              <el-input v-model="formInline.user" placeholder="紧急性"/>
+            </el-form-item>
+            <el-form-item label="紧急性查询">
+              <el-select v-model="formInline.region" placeholder="类别查询">
+                <el-option label="紧急性1" value="紧急性1"/>
+                <el-option label="紧急性2" value="紧急性2"/>
+                <el-option label="紧急性3" value="紧急性3"/>
+                <el-option label="紧急性4" value="紧急性4"/>
+                <el-option label="紧急性5" value="紧急性5"/>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="searchTasks">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </el-row>
 
-      <el-row>
-        <el-col>
-          <el-table :data="tableData.filterData" style="width: 100%" class="table">
-            <el-table-column fixed prop="name" label="任务" width="150px"/>
-            <el-table-column prop="requirements" label="需要资源数量" width="150px"/>
-            <el-table-column prop="products" label="预估产量" width="150px"/>
-            <el-table-column prop="deadline" label="截止时间" width="150px"/>
-            <el-table-column prop="lastTime" label="预估耗时" width="150px"/>
-            <el-table-column prop="numbers" label="参与企业个数" width="150px"/>
-          </el-table>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-pagination @size-change="handleSizeChange"
-                       @current-change="handleCurrentChange"
-                       :current-page="state.currentPage"
-                       :page-sizes="state.pageSizes"
-                       :page-size="state.PageSize" layout="total, sizes, prev, pager, next, jumper"
-                       :total="state.totalCount">
-        </el-pagination>
-      </el-row>
-    </el-tab-pane>
-  </el-tabs>
+        <el-row>
+          <el-col>
+            <el-table :data="tableData.filterData" style="width: 100%" class="table">
+              <el-table-column fixed prop="name" label="任务" width="150px"/>
+              <el-table-column prop="requirements" label="需要资源数量" width="150px"/>
+              <el-table-column prop="products" label="预估产量" width="150px"/>
+              <el-table-column prop="deadline" label="截止时间" width="150px"/>
+              <el-table-column prop="lastTime" label="预估耗时" width="150px"/>
+              <el-table-column prop="numbers" label="参与企业个数" width="150px"/>
+            </el-table>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-pagination @size-change="handleSizeChange"
+                         @current-change="handleCurrentChange"
+                         :current-page="state.currentPage"
+                         :page-sizes="state.pageSizes"
+                         :page-size="state.PageSize" layout="total, sizes, prev, pager, next, jumper"
+                         :total="state.totalCount">
+          </el-pagination>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
